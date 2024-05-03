89bc3c192b3f0ea3b24fe75d003128ed1adbc6dc
my name <blueshaperd3@gmail.com>
2024-05-03 22:00:47 +0500

    replace paragraf

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
+// Replace the paragraf with another paragraf
 function readMore(){
 var text = document.getElementById("text").innerHTML;
 var para = document.getElementById("para")
 para.innerHTML = text
 }
-
-
