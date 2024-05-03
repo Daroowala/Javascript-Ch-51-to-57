78d2e284cdfb45fa947e11225c80e7cc5f0a283f
my name <blueshaperd3@gmail.com>
2024-05-03 22:28:47 +0500

    creat readmore link

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ var text = document.getElementById("text").innerHTML;
 var para = document.getElementById("para")
 para.innerHTML = text
 }
+
+// creat a readmore link
+function read(){
+    var lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate porro repudiandae corrupti placeat obcaecati laudantium tempora mollitia cumque quia saepe? Voluptatum, nam dolores eos id ipsa magnam reiciendis iste distinctio asperiores corrupti accusantium dolorum vitae natus quaerat magni impedit rerum molestiae doloremque perspiciatis alias illum possimus eligendi quo! Non,"
+    var paragraf = document.getElementById("paragraf")
+    paragraf.innerHTML = lorem
+}
